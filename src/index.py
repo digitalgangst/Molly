34e10334ef2fbc19e4d90a96255d0e5a09932c30
g4rcez <allan.f.garcez@gmail.com>
2020-03-29 23:19:05 -0300

    Fix regex for replace like sed unix command

--- src/index.py
+++ src/index.py
@@ -7,7 +7,6 @@ load_dotenv()
 
 botToken = os.getenv('BOTTOKEN')
 
-
 bot = telebot.TeleBot(botToken)
 
 
@@ -18,11 +17,11 @@ def send_welcome(message):
 # Regex Replace
 @bot.message_handler(func=lambda message: message.reply_to_message)
 def replace_regex_msg(message):
-    data = re.search("s\/(.*?)\/(.*?)\/", message.text)
+    data = re.search("s\/(.*?)\/(.*?)\/?$", message.text.strip())
     if not data: # valida a regex
-        return 
+        return
     message_foredit = message.reply_to_message # mensagem a ser editada
-    msg_origin, msg_sub = data.group(1), data.group(2) 
+    msg_origin, msg_sub = data.group(1), data.group(2)
     try: # valida a regex do usuario
         msg = re.sub(msg_origin, msg_sub, message_foredit.text)
         bot.reply_to(message.reply_to_message, "*Você quis dizer:* \n \"{0}\" ".format(msg), parse_mode="Markdown")
