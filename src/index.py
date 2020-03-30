7be2fd94026c717a85f0c3a55ef3a874f9c9c50b
Kaio Gomes <kaiogomes.nave@gmail.com>
2020-03-29 21:09:31 -0300

    Códigos iniciais

--- src/index.py
+++ src/index.py
@@ -2,6 +2,8 @@ import telebot
 from dotenv import load_dotenv
 import os
 
+from messageHandlers.start import sendStart
+
 load_dotenv()
 
 botToken = os.getenv('BOTTOKEN')
@@ -9,9 +11,9 @@ botToken = os.getenv('BOTTOKEN')
 bot = telebot.TeleBot(botToken)
 
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, 'Howdy, how are you doing')
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    print(message)
 
 
 bot.polling()
