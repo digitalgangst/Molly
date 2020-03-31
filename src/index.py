a88684dd5231129d2ee8fd56b0b17eae61a049d4
g4rcez <allan.f.garcez@gmail.com>
2020-03-30 22:35:18 -0300

    modify entrypoint to use replyMessage multi commands

--- src/index.py
+++ src/index.py
@@ -1,13 +1,20 @@
-import telebot
-from dotenv import load_dotenv
 import os
 import re
+import telebot
+
+
+from dotenv import load_dotenv
+from commands.reply.sedCommand import SedCommand
+
+from core.command import AuthCommand
 
 load_dotenv()
 
 botToken = os.getenv('BOTTOKEN')
 
 bot = telebot.TeleBot(botToken)
+
+replyCommands = [SedCommand]
 
 
 @bot.message_handler(commands=['isalive'])
@@ -17,15 +24,11 @@ def send_welcome(message):
 # Regex Replace
 @bot.message_handler(func=lambda message: message.reply_to_message)
 def replace_regex_msg(message):
-    data = re.search("s\/(.*?)\/(.*?)\/?$", message.text.strip())
-    if not data: # valida a regex
-        return
-    message_foredit = message.reply_to_message # mensagem a ser editada
-    msg_origin, msg_sub = data.group(1), data.group(2)
-    try: # valida a regex do usuario
-        msg = re.sub(msg_origin, msg_sub, message_foredit.text)
-        bot.reply_to(message.reply_to_message, "*Você quis dizer:* \n \"{0}\" ".format(msg), parse_mode="Markdown")
-    except re.error:
-        bot.reply_to(message.reply_to_message, "*E:* regex error", parse_mode="Markdown")
+    c = AuthCommand()
+    for command in replyCommands:
+        cmd = command()
+        cmd.load(bot)
+        cmd.exec(message)
+
 
 bot.polling()
