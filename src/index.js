1db9bd61a05fbfe74302c4b1aec03cad5e6f84c6
darko <tintx@protonmail.com>
2020-04-14 22:57:50 -0300

    node

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-const sed = require("./commands/sed");
-const onFire = require("./commands/on-fire");
-const echo = require("./commands/echo");
+process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require("node-telegram-bot-api");
+const fs = require('fs');
+const glob = require('glob');
 require("dotenv").config();
 
 const TOKEN = process.env.BOT_TOKEN;
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-// on text event trigger
-[sed, onFire, echo].forEach((x) => {
-  const cmd = x(bot);
-  bot.onText(cmd.pattern, cmd.command);
+options = {
+  cwd: 'src/commands'
+};
+
+glob("*.command.js", options, (err, files) => {
+  if(err) throw new Error(err);
+  files.map(file => {
+    const func = require(`./commands/${file}`);
+    const cmd = func(bot);
+    bot.onText(cmd.pattern, cmd.command);
+  });
 });
+
